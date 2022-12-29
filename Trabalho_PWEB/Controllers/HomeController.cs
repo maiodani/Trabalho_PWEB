d949c333306e05a8a15885a0c2767c3ddfe33027
tiagofaria2001 <TiagoF2001@gmail.com>
2022-12-29 17:57:15 +0000

    Filtro por Categoria Feito.Reseva por utlizador feito.

--- Trabalho_PWEB/Controllers/HomeController.cs
+++ Trabalho_PWEB/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Trabalho_PWEB.Data;
 using Trabalho_PWEB.Models;
@@ -9,12 +10,11 @@ namespace Trabalho_PWEB.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
-
         public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _context = context;
             _logger = logger;
-        }
+    }
 
         public IActionResult Index(string? msg) {
             PesquisaReservaModelView prmv = new PesquisaReservaModelView();
@@ -28,12 +28,8 @@ namespace Trabalho_PWEB.Controllers
         }
         public async Task<IActionResult> Search([Bind("Localizacao")]PesquisaReservaModelView prmv,string DataLevantamento, string DataEntrega, int cat)
         {
-            if (prmv.Localizacao == null || DataEntrega == null || DataLevantamento == null)
-            {
-                return RedirectToAction("Index","Home",new { msg = "Preecha todos os campos!" });
-            }else{
-
-            }
+            if (prmv.Localizacao == null ||DataEntrega == null || DataLevantamento == null)return RedirectToAction("Index","Home",new { msg = "Preecha todos os campos!" });
+            
             prmv.DataEntrega = new DateOnly();
             prmv.DataEntrega = DateOnly.Parse(DataEntrega);
             prmv.DataLevantamento = new DateOnly();
@@ -42,15 +38,15 @@ namespace Trabalho_PWEB.Controllers
             List<Reservas> r = _context.Reservas.ToList();
             List<Veiculo> veiculosComReserva = new List<Veiculo>();
             foreach (var item in r){ //VAI ADICIONAR A LISTA TODAS AS RESERVAS QUE ESTAO NA TIME FRAME QUE O UTLIZADOR INTRODUZIU
-                if (item.DataLevantamento.Date.CompareTo(prmv.DataLevantamento) >= 0 && item.DataEntrega.Date.CompareTo(prmv.DataEntrega) <= 0){
+                if (DateOnly.FromDateTime(item.DataLevantamento).CompareTo(prmv.DataLevantamento) >= 0 && DateOnly.FromDateTime(item.DataEntrega).CompareTo(prmv.DataEntrega) <= 0){
                     veiculosComReserva.Add(_context.Veiculo.Where(v => v.Id == item.IdVeiculo).First());
                     System.Diagnostics.Debug.WriteLine("ESTA DENTRO DA TIME FRAME!!!");
                 }else{
-                    if (item.DataLevantamento.Date.CompareTo(prmv.DataLevantamento) >= 0 && item.DataLevantamento.Date.CompareTo(prmv.DataEntrega) <= 0){
+                    if (DateOnly.FromDateTime(item.DataLevantamento).CompareTo(prmv.DataLevantamento) >= 0 && DateOnly.FromDateTime(item.DataLevantamento).CompareTo(prmv.DataEntrega) <= 0){
                         veiculosComReserva.Add(_context.Veiculo.Where(v => v.Id == item.IdVeiculo).First());
                         System.Diagnostics.Debug.WriteLine("ESTA DENTRO DA TIME FRAME!!");
                     }else{
-                        if (item.DataEntrega.Date.CompareTo(prmv.DataLevantamento) >= 0 && item.DataEntrega.Date.CompareTo(prmv.DataEntrega) <= 0){
+                        if (DateOnly.FromDateTime(item.DataEntrega).CompareTo(prmv.DataLevantamento) >= 0 && DateOnly.FromDateTime(item.DataEntrega).CompareTo(prmv.DataEntrega) <= 0){
                             veiculosComReserva.Add(_context.Veiculo.Where(v => v.Id == item.IdVeiculo).First());
                             System.Diagnostics.Debug.WriteLine("ESTA DENTRO DA TIME FRAME!!!");
                         }
@@ -58,18 +54,56 @@ namespace Trabalho_PWEB.Controllers
                 }
             }
             List<Veiculo> v = new List<Veiculo>();
-            v = _context.Veiculo.Where(v => v.Ocupado == false).Where(v=> v.Ativo == true).Where(v => v.Localização == prmv.Localizacao).Where(v => v.CategoriaID == cat).ToList();
-            foreach(var item in veiculosComReserva){
+            if (cat == 0)
+            {
+                v = _context.Veiculo.Where(v => v.Ocupado == false).Where(v => v.Ativo == true).Where(v => v.Localização == prmv.Localizacao).ToList();
+            }else{
+                v = _context.Veiculo.Where(v => v.Ocupado == false).Where(v => v.Ativo == true).Where(v => v.Localização == prmv.Localizacao).Where(v => v.CategoriaID == cat).ToList();
+            }
+            foreach (var item in veiculosComReserva){
                 if (v.Contains(item)) v.Remove(item);
             }
             foreach(var item in v)
             {
                 item.Categoria = _context.Categoria.Where(c => c.Id == item.CategoriaID).First();
             }
-            return View("List", v );
+            
+            ViewBag.c = _context.Categoria.ToList();
+            ListVeiculosViewModel lvvm = new ListVeiculosViewModel();
+            lvvm.empresa = new List<Empresa>();
+            foreach (var item in v)
+            {
+                lvvm.empresa.Add(_context.Empresa.Where(e => e.Id == item.EmpresaId).First());
+            }
+            lvvm.veiculos = v;
+            lvvm.DataEntrega = prmv.DataEntrega.ToString();
+            lvvm.DataLevantamento = prmv.DataLevantamento.ToString();
+            lvvm.Localizacao = prmv.Localizacao;
+            return View("List", lvvm);
+        }
+
+        public IActionResult Filtar(int cat, ListVeiculosViewModel lvvm) {
+            PesquisaReservaModelView prmv = new PesquisaReservaModelView();
+            prmv.Localizacao = lvvm.Localizacao;
+            return RedirectToAction("Search", new { prmv.Localizacao, lvvm.DataLevantamento, lvvm.DataEntrega, cat });
+        }
+        [Authorize(Roles = "Admin,Cliente")]
+
+        public async Task<IActionResult> Reservar(int id,string dataE,string dataL)
+        {
+            Reservas r = new Reservas();
+            r.idReservante = _context.Users.Where(u => u.Email == User.Identity.Name).Select(u => u.Id).First();
+            r.IdVeiculo = _context.Veiculo.Where(v => v.Id == id).Select(v => v.Id).First();
+            r.Veiculo = _context.Veiculo.Where(v => v.Id == id).First();
+            r.DataLevantamento = DateTime.Parse(dataL);
+            r.DataEntrega = DateTime.Parse(dataE);
+            r.Ativa = false;
+            _context.Add(r);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
         public async Task<IActionResult> List(List<Veiculo> v){
-            return View(v);
+            return View("Index");
         }
 
         public IActionResult Privacy()
---
+++ Trabalho_PWEB/Models/ListVeiculosViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trabalho_PWEB.Models
+{
+    public class ListVeiculosViewModel
+    {
+        [Required]
+        public List<Veiculo> veiculos { get; set; }
+        [Required]
+        public List<Empresa> empresa { get; set; }
+        [Required]
+        public string Localizacao { get; set; }
+        [Required]
+        public string DataLevantamento { get; set; }
+        [Required]
+        public string DataEntrega { get; set; }
+    }
+}
